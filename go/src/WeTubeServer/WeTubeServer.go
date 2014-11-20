c6bec3034c7ed5a50a0d61c3c4827a2168e5bc16
imgemp <imgemp@haar.cs.umass.edu>
2014-11-20 17:41:20 -0500

    Added WeTubeServer (HTTP) Server
    - WeTubeServer [enter] from 630Systems directory to start server
    - downloaded websocket package

---
+++ go/src/WeTubeServer/WeTubeServer.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func main() {
+	fmt.Printf("Starting server at http://localhost:8080/\n")
+    http.Handle("/", http.FileServer(http.Dir("./html/")))
+    http.ListenAndServe(":8080", nil)
+}
